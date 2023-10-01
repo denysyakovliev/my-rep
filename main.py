e2badb524cd0a7279a3ad025f88cd82697c6b30d
Denys Y <den6175@gmail.com>
2023-10-01 19:27:47 +0300

    Game creat

--- main.py
+++ main.py
@@ -1,5 +1,57 @@
+import re
+import random
+
+words = ["hello", "work", "bike", "python"]
+selection_word = random.choice(words)
+attempts = input("Enter numbers of try:\n")
+if not isinstance(attempts, int):
+    attempts = int(input('Please enter a number value of tries:\n'))
+attempts = int(attempts)
+part_pattern = ""
+count = 0
+
+while True:
+
+    if count == attempts:
+        print("You lose")
+        exit()
+
+    elem = input("Enter letter or word: ")
+
+    if len(elem) == 1:
+
+        if elem in selection_word:
+
+            part_pattern += elem
+
+            pattern = f"[^{part_pattern}]"
+
+            print(re.sub(pattern, "*", selection_word))
+            if re.sub(pattern, "*", selection_word) == selection_word:
+                print('You won!')
+            continue
+
+        else:
+
+            print("Invalid letter")
+
+            count += 1
+
+            continue
 
 
-if __name__ == "__main__":
-    print('Hi')
-  
+
+    else:
+
+        if elem == selection_word:
+
+            print("You won")
+
+            exit()
+
+        else:
+
+            print("Invalid word")
+
+            count += 1
+
